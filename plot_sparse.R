c62cbd9d34463d399080e675622ce93f6c692c3d
Victoria DeLeo <vldeleo@users.noreply.github.com>
2021-06-01 22:40:15 -0400

    code for visualizing simulated models

---
+++ plot_sparse.R
@@ -0,0 +1,54 @@
+library(ggplot2)
+library(ggridges)
+library(reshape2)
+
+# how well do different models do at predicting unobserved values
+predR2many <- read.csv("many_PredR2.csv")
+predR2few <- read.csv("few_PredR2.csv")
+predR2many$sparse <- "dense"
+predR2few$sparse <- "sparse"
+predR2 <- rbind(predR2many, predR2few)
+predR2$X <- as.character(predR2$X) # turns out I can't use an integer as an ID column
+predR2 <- melt(predR2, id = c("X", "sparse"), variable.name = "model")
+ggplot(predR2, aes(x = model, y = value, color = sparse, fill = sparse)) +  geom_boxplot(width=0.5,lwd=1) + ggtitle("R2 of Predicted vs Actual OOS") + theme_bw()
+
+# how well do different models do at finding Betas
+BetaR2many <- read.csv("many_BetaR2.csv")
+BetaR2few <- read.csv("few_BetaR2.csv")
+BetaR2many$sparse <- "dense"
+BetaR2few$sparse <- "sparse"
+BetaR2 <- rbind(BetaR2many, BetaR2few)
+BetaR2$X <- as.character(BetaR2$X) # turns out I can't use an integer as an ID column
+BetaR2 <- melt(BetaR2, id = c("X", "sparse"), variable.name = "model")
+ggplot(BetaR2, aes(x = model, y = value, color = sparse, fill = sparse)) +  geom_boxplot(width=0.5,lwd=1) + ggtitle("R2 of Model vs Actual Beta") + theme_bw()
+
+
+
+
+# how about distributions of R2?
+# and better yet, how distributions of R2 change from few to many predictors for each model
+
+ggplot(predR2, aes(x = Variable, y = model, color = sparse, point_color = sparse, fill = sparse)) +
+  geom_density_ridges(
+    jittered_points = TRUE, scale = .95, rel_min_height = .01,
+    point_shape = "|", point_size = 3, size = 0.25,
+    position = position_points_jitter(height = 0)
+  ) +
+  scale_y_discrete(expand = c(0, 0)) +
+  scale_x_continuous(expand = c(0, 0), name = "r^2") +
+  scale_fill_manual(values = c("darkblue", "darkgold"), labels = c("sparse", "dense")) +
+  scale_color_manual(values = c("darkblue", "darkgold"), guide = "none") +
+  #scale_discrete_manual("point_color", values = c("#D55E00", "#0072B2"), guide = "none") +
+  coord_cartesian(clip = "off") +
+  guides(fill = guide_legend(
+    override.aes = list(
+      fill = c("darkblue", "darkgold"),
+      color = NA, point_color = NA)
+  )
+  ) +
+  ggtitle("r^2 Predictions of OOS") +
+  theme_ridges(center = TRUE)
+
+png(filename = "R2dens_5.28.21.png", width = 600, height = 400)
+density(predR2tog, predR2tog)
+dev.off()
--- viz_sparseSim.R
+++ viz_sparseSim.R
@@ -8,45 +8,205 @@
 # set up R
 library(rethinking)
 library(dplyr)
+library(reshape2)
 
-# compile my data
-yfew_files <- list.files(pattern = "simy.csv", full.names=TRUE) 
-#test <- read.csv(allfiles[1], header = TRUE)
-yfew_list = lapply(yfew_files, function(x){read.csv(file=x,header=T)})
-Reduce(function(x,y) {merge(x,y, by = "row.names")}, yfew_list)
-  # ^ instead of this, use the code I figured out below
+# compile my data for Betas
+simBeta_fn <- list.files(pattern = "simBeta.csv", full.names=TRUE)
+Betafew_data = lapply(simBeta_fn, function(x){read.csv(file=x,header=T)})
+simBetaall <- Reduce(function(x,y){full_join(x,y, by = "X")}, Betafew_data)
+colnames(simBetaall) <- c("X", paste("rep",1:100, sep = ""))
+#
+lmBeta_fn <- list.files(pattern = "lmBeta.csv", full.names=TRUE)
+lmBeta_data = lapply(lmBeta_fn, function(x){read.csv(file=x,header=T)})
+lmBetaall <- Reduce(function(x,y){full_join(x,y, by = "X")}, lmBeta_data)
+colnames(lmBetaall) <- c("X", paste("rep",1:100, sep = ""))
+#
+susBeta_fn <- list.files(pattern = "susBeta.csv", full.names=TRUE)
+susBeta_data = lapply(susBeta_fn, function(x){read.csv(file=x,header=T)})
+susBetaall <- Reduce(function(x,y){full_join(x,y, by = "X")}, susBeta_data)
+colnames(susBetaall) <- c("X", paste("rep",1:100, sep = ""))
+#
+blasBeta_fn <- list.files(pattern = "blasBeta.csv", full.names=TRUE)
+blasBeta_data = lapply(blasBeta_fn, function(x){read.csv(file=x,header=T)})
+blasBetaall <- Reduce(function(x,y){full_join(x,y, by = "X")}, blasBeta_data)
+colnames(blasBetaall) <- c("X", paste("rep",1:100, sep = ""))
+#
+bhsBeta_fn <- list.files(pattern = "bhsBeta.csv", full.names=TRUE)
+bhsBeta_data = lapply(bhsBeta_fn, function(x){read.csv(file=x,header=T)})
+bhsBetaall <- Reduce(function(x,y){full_join(x,y, by = "X")}, bhsBeta_data)
+colnames(bhsBetaall) <- c("X", paste("rep",1:100, sep = ""))
+#
+briBeta_fn <- list.files(pattern = "bridgeBeta.csv", full.names=TRUE)
+briBeta_data = lapply(briBeta_fn, function(x){read.csv(file=x,header=T)})
+briBetaall <- Reduce(function(x,y){full_join(x,y, by = "X")}, briBeta_data)
+colnames(briBetaall) <- c("X", paste("rep",1:100, sep = ""))
+#
 
+
+### pull up the data used for OOS predictions ###
 #
 yOOSfew_fn <- list.files(pattern = "yOOS.csv", full.names=TRUE) 
 yOOSfew_data <- lapply(yOOSfew_fn, function(x){read.csv(file=x,header=T)})
 yOOSall <- Reduce(function(x,y){full_join(x,y, by = "X")}, yOOSfew_data)
 colnames(yOOSall) <- c("X", paste("rep",1:100, sep = ""))
-
 #
 lmPredfew_fn <- list.files(pattern = "lmPred.csv", full.names=TRUE) 
 lmPredfew_data = lapply(lmPredfew_fn, function(x){read.csv(file=x,header=T)})
 lmPredall <- Reduce(function(x,y){full_join(x,y, by = "X")}, lmPredfew_data)
 colnames(lmPredall) <- c("X", paste("rep",1:100, sep = ""))
+#
+susPredfew_fn <- list.files(pattern = "susPred.csv", full.names=TRUE) 
+susPredfew_data = lapply(susPredfew_fn, function(x){read.csv(file=x,header=T)})
+susPredall <- Reduce(function(x,y){full_join(x,y, by = "X")}, susPredfew_data)
+colnames(susPredall) <- c("X", paste("rep",1:100, sep = ""))
+#
+blasPredfew_fn <- list.files(pattern = "blasPred.csv", full.names=TRUE) 
+blasPredfew_data = lapply(blasPredfew_fn, function(x){read.csv(file=x,header=T)})
+blasPredall <- Reduce(function(x,y){full_join(x,y, by = "X")}, blasPredfew_data)
+colnames(blasPredall) <- c("X", paste("rep",1:100, sep = ""))
+#
+bhsPredfew_fn <- list.files(pattern = "bhsPred.csv", full.names=TRUE) 
+bhsPredfew_data = lapply(bhsPredfew_fn, function(x){read.csv(file=x,header=T)})
+bhsPredall <- Reduce(function(x,y){full_join(x,y, by = "X")}, bhsPredfew_data)
+colnames(bhsPredall) <- c("X", paste("rep",1:100, sep = ""))
+#
+briPredfew_fn <- list.files(pattern = "bridgePred.csv", full.names=TRUE) 
+briPredfew_data = lapply(briPredfew_fn, function(x){read.csv(file=x,header=T)})
+briPredall <- Reduce(function(x,y){full_join(x,y, by = "X")}, briPredfew_data)
+colnames(briPredall) <- c("X", paste("rep",1:100, sep = ""))
+
+
+
 
 
 # 1. How well do model coefficients match true coefficients?
+# Pearsons?
+  # I'm not sure my data are linear + homoscedastic (actually, almost positive these won't be homoscedastic for the dense simulated model)
+  # if I ranked my effect sizes, I could try Spearman's coefficient- I do hope for a monotonic relationship here.
+shapiroReal <- lapply(2:101,function(rep){shapiro.test(simBetaall[,rep])})
+shapiroLM <- lapply(2:101,function(rep){shapiro.test(lmBetaall[2:401,rep])})
+      # hmmm very non-normal
 
-lmCoefCor <- apply(function(rep){cor.test(simBeta[rep], lmBeta[rep])}, 1:100)
+lmBetaRho <- unlist(lapply(2:101,function(rep){unname(cor.test(simBetaall[,rep], lmBetaall[2:401,rep])$estimate)}))
+susBetaRho <- unlist(lapply(2:101,function(rep){unname(cor.test(simBetaall[,rep], susBetaall[2:401,rep])$estimate)}))
+blasBetaRho <- unlist(lapply(2:101,function(rep){unname(cor.test(simBetaall[,rep], blasBetaall[2:401,rep])$estimate)}))
+bhsBetaRho <- unlist(lapply(2:101,function(rep){unname(cor.test(simBetaall[,rep], bhsBetaall[2:401,rep])$estimate)}))
+briBetaRho <- unlist(lapply(2:101,function(rep){unname(cor.test(simBetaall[,rep], briBetaall[2:401,rep])$estimate)}))
+
+BetaRhotog <- as.data.frame(cbind(lmBetaRho, susBetaRho, blasBetaRho, bhsBetaRho, briBetaRho))
+BetaRholong <- melt(BetaRhotog, variable.name = "Model")
+
+png(filename = "many_BetaRhoboxplot_5.28.21.png", width = 600, height = 400)
+boxplot(value ~ Model, data = BetaRholong, main = "")
+dev.off()
+
+# I wrote the model vs actual df to file so I could upload to github for Rmarkdown
+write.csv(BetaRhotog, "many_BetaRho.csv")
+
+
+# Root Mean Squared Error of Betas
+lmBetaRMSE <- unlist(lapply( 2:101, function(rep){sqrt(mean((summary(lm(lmBetaall[2:401,rep]~simBetaall[,rep]))$residuals)^2))}))
+susBetaRMSE <- unlist(lapply( 2:101, function(rep){sqrt(mean((summary(lm(susBetaall[2:401,rep]~simBetaall[,rep]))$residuals)^2))}))
+blasBetaRMSE <- unlist(lapply( 2:101, function(rep){sqrt(mean((summary(lm(blasBetaall[2:401,rep]~simBetaall[,rep]))$residuals)^2))}))
+bhsBetaRMSE <- unlist(lapply( 2:101, function(rep){sqrt(mean((summary(lm(bhsBetaall[2:401,rep]~simBetaall[,rep]))$residuals)^2))}))
+briBetaRMSE <- unlist(lapply( 2:101, function(rep){sqrt(mean((summary(lm(briBetaall[2:401,rep]~simBetaall[,rep]))$residuals)^2))}))
+# get a warning about the bridge predictions being "essentially perfect fit: summary may be unreliable"
+BetaRMSEtog <- as.data.frame(cbind(lmBetaRMSE, susBetaRMSE, blasBetaRMSE, bhsBetaRMSE, briBetaRMSE))
+BetaRMSElong <- melt(BetaRMSEtog, variable.name = "Model")
+
+png(filename = "many_BetaRMSEboxplot_5.28.21.png", width = 600, height = 400)
+boxplot(value ~ Model, data = BetaRMSElong)
+dev.off()
+
+# writing the file
+write.csv(BetaRMSEtog, "many_BetaRMSE.csv")
+
+
+#r^2
+lmBetaR2 <- unlist(lapply( 2:101, function(rep){summary(lm(lmBetaall[2:401,rep]~simBetaall[,rep]))$adj.r.squared})) # be sure to skip first row of model predictions, which should be the intercept
+susBetaR2 <- unlist(lapply( 1:100, function(rep){summary(lm(susBetaall[2:401,rep+1]~simBetaall[,rep+1]))$adj.r.squared}))
+blasBetaR2 <- unlist(lapply( 1:100, function(rep){summary(lm(blasBetaall[2:401,rep+1]~simBetaall[,rep+1]))$adj.r.squared}))
+bhsBetaR2 <- unlist(lapply( 1:100, function(rep){summary(lm(bhsBetaall[2:401,rep+1]~simBetaall[,rep+1]))$adj.r.squared}))
+briBetaR2 <- unlist(lapply( 1:100, function(rep){summary(lm(briBetaall[2:401,rep+1]~simBetaall[,rep+1]))$adj.r.squared}))
+# 
+BetaR2tog <- as.data.frame(cbind(lmBetaR2, susBetaR2, blasBetaR2, bhsBetaR2, briBetaR2))
+BetaR2long <- melt(BetaR2tog, variable.name = "Model")
+
+png(filename = "many_BetaR2boxplot_5.28.21.png", width = 600, height = 400)
+boxplot(value ~ Model, data = BetaR2long, main = "")
+dev.off()
+
+# I wrote the model vs actual df to file so I could upload to github for Rmarkdown
+write.csv(BetaR2tog, "many_BetaR2.csv")
+
+  # for the models fit to data with many true effects, the r2 may be misleading because off course many effects are set to 0. would be interesting to see how this differs at different effect sizes.
+lmBetaHE <- lmBetaall[lmBetaall > (mean(lmBetall) + sd(lmBetaall)) | lmBetaall < (mean(lmBetall) + sd(lmBetaall))]
+
+lmBetaR2 <- unlist(lapply( 1:100, function(rep){summary(lm(lmBetaall[2:401,rep+1]~simBetaall[,rep+1]))$adj.r.squared})) # be sure to skip first row of model predictions, which should be the intercept
+susBetaR2 <- unlist(lapply( 1:100, function(rep){summary(lm(susBetaall[2:401,rep+1]~simBetaall[,rep+1]))$adj.r.squared}))
+blasBetaR2 <- unlist(lapply( 1:100, function(rep){summary(lm(blasBetaall[2:401,rep+1]~simBetaall[,rep+1]))$adj.r.squared}))
+bhsBetaR2 <- unlist(lapply( 1:100, function(rep){summary(lm(bhsBetaall[2:401,rep+1]~simBetaall[,rep+1]))$adj.r.squared}))
+briBetaR2 <- unlist(lapply( 1:100, function(rep){summary(lm(briBetaall[2:401,rep+1]~simBetaall[,rep+1]))$adj.r.squared}))
+# 
+BetaR2tog <- as.data.frame(cbind(lmBetaR2, susBetaR2, blasBetaR2, bhsBetaR2, briBetaR2))
+BetaR2long <- melt(BetaR2tog, variable.name = "Model")
 
 
 
 
 # 2. How well do model predictions match OOS true response variables?
 
-lmPredCor <- apply(function(rep){cor.test(yOOS[rep], lmPred[rep])}, 1:100)
-susPredCor <- apply(function(rep){cor.test(yOOS[rep], susPred[rep])}, 1:100)
+
+# Root Mean Squared Error of predictions
+lmPredRMSE <- unlist(lapply( 2:101, function(rep){sqrt(mean((summary(lm(lmPredall[,rep]~yOOSall[,rep]))$residuals)^2))}))
+susPredRMSE <- unlist(lapply( 2:101, function(rep){sqrt(mean((summary(lm(susPredall[,rep]~yOOSall[,rep]))$residuals)^2))}))
+susPredRMSE <- unlist(lapply( 2:101, function(rep){sqrt(mean((summary(lm(susPredall[,rep]~yOOSall[,rep]))$residuals)^2))}))
+blasPredRMSE <- unlist(lapply( 2:101, function(rep){sqrt(mean((summary(lm(blasPredall[,rep]~yOOSall[,rep]))$residuals)^2))}))
+bhsPredRMSE <- unlist(lapply( 2:101, function(rep){sqrt(mean((summary(lm(bhsPredall[,rep]~yOOSall[,rep]))$residuals)^2))}))
+briPredRMSE <- unlist(lapply( 2:101, function(rep){sqrt(mean((summary(lm(briPredall[,rep]~yOOSall[,rep]))$residuals)^2))}))
+# get a warning about the bridge predictions being "essentially perfect fit: summary may be unreliable"
+predRMSEtog <- as.data.frame(cbind(lmPredRMSE, susPredRMSE, blasPredRMSE, bhsPredRMSE, briPredRMSE))
+predRMSElong <- melt(predRMSEtog, variable.name = "Model")
+
+png(filename = "many_PredRMSEboxplot_5.28.21.png", width = 600, height = 400)
+boxplot(value ~ Model, data = predRMSElong)
+dev.off()
+
+# writing the file
+write.csv(predRMSEtog, "many_PredRMSE.csv")
 
 
-boxplot(lmPredCor, susPredCor)
+
+# using R2
+lmPredR2 <- unlist(lapply( 1:100, function(rep){summary(lm(lmPredall[,rep+1]~yOOSall[,rep+1]))$adj.r.squared}))
+susPredR2 <- unlist(lapply( 1:100, function(rep){summary(lm(susPredall[,rep+1]~yOOSall[,rep+1]))$adj.r.squared}))
+susPredR2 <- unlist(lapply( 1:100, function(rep){summary(lm(susPredall[,rep+1]~yOOSall[,rep+1]))$adj.r.squared}))
+blasPredR2 <- unlist(lapply( 1:100, function(rep){summary(lm(blasPredall[,rep+1]~yOOSall[,rep+1]))$adj.r.squared}))
+bhsPredR2 <- unlist(lapply( 1:100, function(rep){summary(lm(bhsPredall[,rep+1]~yOOSall[,rep+1]))$adj.r.squared}))
+briPredR2 <- unlist(lapply( 1:100, function(rep){summary(lm(briPredall[,rep+1]~yOOSall[,rep+1]))$adj.r.squared}))
+  # get a warning about the bridge predictions being "essentially perfect fit: summary may be unreliable"
+predR2tog <- as.data.frame(cbind(lmPredR2, susPredR2, blasPredR2, bhsPredR2, briPredR2))
+predR2long <- melt(predR2tog, variable.name = "Model")
+
+png(filename = "many_R2boxplot_5.28.21.png", width = 600, height = 400)
+boxplot(value ~ Model, data = predR2long)
+dev.off()
+
+# I wrote the model vs prediction df to file so I could upload to github for Rmarkdown
+write.csv(predR2tog, "many_PredR2.csv")
 
 
-# using R2 (preferred method)
-lmPredR2 <- lapply( 1:100, function(rep){summary(lm(lmPredall[,rep+1]~yOOSall[,rep+1]))$adj.r.squared})
+
+# 3. really basic, what are the performance stats on the models?
+#Root Mean Squared Error:
+d = simy-pred(lmBeta(simX))
+mse = mean((d)^2)
+mae = mean(abs(d))
+rmse = sqrt(mse)
 
 
-# Brier score? (for classification problems, not relevant)
+# in reverse:
+rmse <- sqrt(mean((simy-)^2))
+
+sqrt(mean(M$residuals^2))
+

8e8dc777aab7ab043db29e1c526bda17573c99ba
Victoria DeLeo <vldeleo@users.noreply.github.com>
2021-06-09 13:38:27 -0400

    drop model fitting step

--- sparseSim.R
+++ sparseSim.R
@@ -1,4 +1,4 @@
-# Competing sparse model approaches across different situations
+# Simulating datasets to compete models on
 
 # Load Libraries
 library(susieR)
@@ -6,21 +6,9 @@ library(rethinking)
 library(MASS) # because I don't have rethinking on the teton server
 library(monomvn)
 library(reshape2)
-library(ggplot2)
 library(clusterGeneration) #a more flexible way to generate covariance matrices
-library(ggpubr) # for plotting in multiple panes
-library(qgraph) # because I'd like cool viz of my correlated predictors
-set.seed(518) #used this for first set
-              # second set (with many effects) I set.seed(527) and then after 30 set.seed(528)
+set.seed(6721) # for n1000p800fewcorrvar10, seed = 6721
 
-# Simulate Data
-  #I think I'll want this to be a function that I can choose options
-  #correlation: high (0-1), low (-.4-.4), no (0)
-    #ncorr: many (50% predictors), few (20% predictors)
-  #effect: high (), mostlylow (see beta definition below), low ()
-    #neffect: many (1/5), few (1/50) (question of sparsity)
-  #how many observations?
-  #how many covariates?
 
 ###for testing purposes, delete when functional###
   n <- 1000 
@@ -29,23 +17,24 @@ set.seed(518) #used this for first set
 ###
 
   
-  
 # I want to run this 500 times to see how well each of these models do under many effect, non-sparse situations with correlated variables of variance 10  
   #this is really inefficient as is; should make a way to not calculate OOS variables if I'm not trying to compare predictions
   
-
-sparseSim <- function(n, p, effect = c("many", "few"), corr = c("no", "yes"), covMeth = c("onion"), Var, sparse = FALSE, model = c("lm", "susie", "blasso", "bhs", "bridge"), M, withPred = TRUE){
+  
+sparseSim <- function(n, p, effect = c("many", "few"), corr = c("no", "yes"), covMeth = c("onion"), Var, sparse = FALSE){
   n <- n #observations
   p <- p #predictors
   Var <- Var
-  M <- M
 
 #create correlation matrix for predictors
 
 #this method is pretty slow compared to other options but allows for more control
   #unifcorrmat is deathly slow. maybe stick to onion as the method.
-Sigma <- genPositiveDefMat("onion", rangeVar = c(0,Var), dim=p)$Sigma
+Sigma <- genPositiveDefMat("onion", rangeVar = c(1,Var), dim=p)$Sigma
   #changing the rangeVar, I can change the allowable range for variances
+# it's possible to change the eta parameter for generating the correlation matrix, either with the c-vine method or the onion method
+# I left it with the default eta=1
+
   
 #### Originally I wanted an option to specify how /many/ predictors are correlated as well as how /strongly/ predictors are correlated
   # I still haven't managed that
@@ -53,7 +42,6 @@ Sigma <- genPositiveDefMat("onion", rangeVar = c(0,Var), dim=p)$Sigma
 
 #simulate values of X
 #to test predictive ability, let me simulate an OOS population
-  
   if (corr == "no"){
     Xfull <- matrix(rnorm((3*n/2)*p, 0, 1),nrow=(3*n/2),ncol=p)
   } else if (corr == "yes") {   # Sigma will be different for high/low based on code above
@@ -67,10 +55,7 @@ Sigma <- genPositiveDefMat("onion", rangeVar = c(0,Var), dim=p)$Sigma
 
 # now, go back to the appropriately sized X
   #X is dim n*p
-
 X <- Xfull[1:n,]
-
-
 
   
 #simulate effects
@@ -82,7 +67,7 @@ if (effect == "many"){
 }
   
 
-#adjust sparsity (the above code for Beta causes sparsity as well, this is just more explicit)
+#adjust sparsity (the above code for Beta approximates sparsity as well, this is just more explicit)
 if (sparse == TRUE) { # if most predictors really don't have any effect, they should == 0
   beta <- beta
   beta[sample(c(1:p), p*(9/10), replace = F)] <- 0 # here, only 1/10 of predictors are allowed to be non-zero
@@ -94,59 +79,14 @@ yfull <- Xfull %*% beta + rnorm(3*n/2)
 y <- yfull[1:n,] # this is my smaller, "sampled" population
 
 
-#fit model
-  # I think I would like to have the option to test multiple models on the same simulated data at once
-if ("lm" %in% model) {
-lmres <- lm(y ~ X)
-}
-if ("susie" %in% model ) {
-susres <- susie(X, y, L=M)
-}
-if ("blasso" %in% model) {
-blassores <- blasso(X, y, RJ=TRUE, M=M, T=15000, verb = 0)
-}
-if ("bhs" %in% model) {
-bhsres <- bhs(X, y, RJ=TRUE, M=M, T=15000, verb = 0)
-}
-if ("bridge" %in% model) {
-bridgeres <- bridge(X, y, RJ=TRUE, M=M, T=15000, verb = 0)
-}
-
-
-#predict y from models
-if (withPred == TRUE) {
-if(exists("lmres")){
-  lmpred <- lmres$coefficients[1] + Xfull[(n+1):nrow(Xfull),] %*% lmres$coefficients[-1] 
-    #note: R appears to still not have a good way to handle predictions of different shape than initial data, so I tried to do it the old fashioned way
-}
-if(exists("susres")){
-  suspred <- coef(susres)[1] + Xfull[(n+1):nrow(Xfull),] %*% coef(susres)[-1]
-  #suspred <- predict(susres, newdata = Xfull[(n+1):nrow(Xfull),])
-    #going back to my note above, predict doesn't work with a differently shaped NewData. I tried doubling my oos so it would be the same size, but it looks like pred still used x values from my original set. GAH.
-}
-if(exists("blassores")){
-  blassopred <- mean(blassores$mu[5001:15000]) + Xfull[(n+1):nrow(Xfull),] %*% colMeans(blassores$beta[5001:15000,]) 
-}
-if(exists("bhsres")){
-  bhspred <- mean(bhsres$mu[5001:15000]) + Xfull[(n+1):nrow(Xfull),] %*% colMeans(bhsres$beta[5001:15000,]) 
-}
-if(exists("bridgeres")){
-  bridgepred <- mean(bridgeres$mu[5001:15000]) + Xfull[(n+1):nrow(Xfull),] %*% colMeans(bridgeres$beta[5001:15000,]) 
-}
-}
 
 #get output
 out <- list(y, X, beta, 
-            if(exists("yfull")){yfull[(n+1):nrow(yfull)]}, if(exists("Xfull")){Xfull[(n+1):nrow(Xfull),]},
-            if(exists("lmres")){coef(lmres)}, if(exists("lmpred")){lmpred},
-            if(exists("susres")){coef(susres)}, if(exists("suspred")){suspred},
-            if(exists("blassores")){c(mean(blassores$mu[5001:15000]),colMeans(blassores$beta[5001:15000,]))}, if(exists("blassopred")){blassopred},
-            # now, I'll have intercept and coefficients, in same form as lm and susie
-            if(exists("bhsres")){c(mean(bhsres$mu[5001:15000]),colMeans(bhsres$beta[5001:15000,]))}, if(exists("bhspred")){bhspred},
-            if(exists("bridgeres")){c(mean(bridgeres$mu[5001:15000]),colMeans(bridgeres$beta[5001:15000,]))}, if(exists("bridgepred")){bridgepred}
-            )
+            if(exists("yfull")){yfull[(n+1):nrow(yfull),]}, 
+            if(exists("Xfull")){Xfull[(n+1):nrow(Xfull),]}
+)
 
-names(out) <- c("simy", "simX", "simBeta", "yOOS", "XOOS", "lmBeta", "lmPred", "susBeta", "susPred", "blasBeta", "blasPred", "bhsBeta", "bhsPred", "bridgeBeta", "bridgePred")
+names(out) <- c("simy", "simX", "simBeta", "yOOS", "XOOS")
 out <- out[!sapply(out,is.null)]
 return(out)  
 }
@@ -155,27 +95,17 @@ return(out)
 ### LOOP CODE ###
 
 AllOutput <- list()
-# use set.seed 529 for 61-75
-# set.seed(530) for 76-
-for (i in 76:100) {
-  stepOut <- sparseSim(500, 400, effect = "many", corr = "yes", Var = 10, model = c("lm", "susie", "blasso", "bhs", "bridge"), M = 15, withPred = TRUE)
+#for (obs in c(100, 500, 1000, 5000)){ 
+  # it would be nice to set and forget, but I'd need to write it in a way that puts the output in different folders if I were to do all in a loop
+for (i in 1:500) {
+  stepOut <- sparseSim(1000, 800, effect = "few", corr = "yes", Var = 10)
   repname <- paste("rep", i, sep = "")
-  #AllOutput[[repname]] <- stepOut
   print(repname)
   for (l in 1:length(stepOut)){
     write.csv(stepOut[l], file = paste(repname, "_", names(stepOut)[l], ".csv", sep = ""))
   }
 }
-save(AllOutput, file = "n1000p800fewcorrvar10m15_2021.5.21.Rdata")
-# this is crazy slow and will never finish in time ):
+#}
+#save(AllOutput, file = "filename.Rdata")
     # added a write step within the loop in case I hit a wall limit
-    # writing reps individually allows me to stay under the default memory limit for my interactive session
-#for n = 1000 and p = 800, this takes about 5 min/rep
-  # can do 40 reps in <4 hours, which seems like the best choice for now
-
-
-
-#I am afraid something weird is going on with the lmres coefficients returned in my function
-  #but admittedly lm() could just be a terrible way to model the data
-
-
+    # writing reps individually also allows me to stay under the default memory limit

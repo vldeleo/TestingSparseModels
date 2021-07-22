dc130ca9f0525e45d5b728ddda4ac2ff09660869
Victoria DeLeo <vldeleo@users.noreply.github.com>
2021-07-22 13:09:16 -0400

    testing key

--- sparseFit.R
+++ sparseFit.R
@@ -3,11 +3,16 @@
   # things I would like to add but haven't yet:
     # BSLMM
     # RJ vs non-RJ
-    # non-bayesian lasso
+
+### load modules ###
+#module load swset/2018.05  gcc/7.3.0 py-matplotlib/2.2.2-py36 r/4.0.5-py27
+
+#try using -m 50M with salloc?
 
 ### load libraries ###
 library(susieR)
 library(monomvn)
+library(glmnet)
 library(dplyr)
 library(reshape2)
 library(clusterGeneration) #a more flexible way to generate covariance matrices
@@ -16,8 +21,9 @@ library(clusterGeneration) #a more flexible way to generate covariance matrices
 resFiles <- c(list.files(pattern = "simy.csv", full.names=TRUE)) # create list of files containing my response variables
 resData <- lapply(resFiles, function(x){read.csv(file=x,header=T)})
 resAll <- Reduce(function(x,y){full_join(x,y, by = "X")}, resData) # combine all response files into a single dataframe
-colnames(resAll) <- c("X", paste("rep",1:ncol(resAll), sep = "")) # a dataframe with rnows of predicted responses (individuals) over ncolumns + 1 (reps) 
+colnames(resAll) <- c("X", paste("rep",1:(ncol(resAll)-1), sep = "")) # a dataframe with rnows of predicted responses (individuals) over ncolumns + 1 (reps) 
 
+  #if working with a large number of predictors, this can run out of 
 predFiles <- c(list.files(pattern = "simX.csv", full.names=TRUE)) # create list of files containing my predictor variables
 predData <- lapply(predFiles, function(x){read.csv(file=x,header=T)}) # a list of dataframes... values for multiple predictors for every individual across all reps (list indices)
 names(predData) <- c(paste("rep",1:length(predData), sep = ""))
@@ -42,7 +48,7 @@ names(XOOSData) <- c(paste("rep",1:length(XOOSData), sep = ""))
 
 
 ### define modeling function ###
-fitMod <- function(X, y, M=NULL, yOOS=NULL, XOOS=NULL, model = c("lm", "susie", "blasso", "bridge", "bhs"), withPred=TRUE){
+fitMod <- function(X, y, M=NULL, yOOS=NULL, XOOS=NULL, model = c("lm", "susie", "blasso", "bridge", "bhs", "lasso",), withPred=TRUE){
   X <- X
   y <- y
   if(!is.null(M)){ M <- M }# can set max expected non-zero predictors; if sparsity is expected, lower values are faster. 
@@ -83,25 +89,56 @@ if (withPred == TRUE){
   
 ### fitting the models ###  
 if ("lm" %in% model) {
-  if(exists(Xtest)){lmres <- lm(ytest ~ Xtest) }
-  else {lmres <- lm(y ~ X) }
+  pt1 <- proc.time() # I'd like to compare how long each model takes
+  if(exists(Xtest)){
+    lmres <- lm(ytest ~ Xtest) } else {
+      lmres <- lm(y ~ X) }
+  lmTime <- proc.time() - pt1
 }
 if ("susie" %in% model ) {
-  if(exists(Xtest)){susres <- susie(as.matrix(Xtest[,-1]), as.matrix(ytest), L=M)}
-  else { susres <- susie(as.matrix(X[,-1]), as.matrix(y), L=M) }
+  pt1 <- proc.time()
+  if(exists(Xtest)){
+    susres <- susie(as.matrix(Xtest[,-1]), as.matrix(ytest), L=M)} else { 
+      susres <- susie(as.matrix(X[,-1]), as.matrix(y), L=M) }
+  susTime <- proc.time() - pt1
 }
 if ("blasso" %in% model) {
-  if(exists(Xtest)){blassores <- blasso(Xtest[,-1], ytest, RJ=FALSE, M=M, T=25000, verb = 0)} else {blassores <- blasso(X[,-1], y, RJ=FALSE, M=M, T=25000, verb = 0)}
+  pt1 <- proc.time()
+  if(exists(Xtest)){
+    blassores <- blasso(Xtest[,-1], ytest, RJ=FALSE, M=M, T=25000, verb = 0)
+  } else {blassores <- blasso(X[,-1], y, RJ=FALSE, M=M, T=25000, verb = 0)}
+  blasTime <- proc.time() - pt1
 }
 if ("bhs" %in% model) {
+  pt1 <- proc.time()
   if(exists(Xtest)){bhsres <- bhs(Xtest[,-1], ytest, RJ=FALSE, M=M, T=25000, verb = 0)} else { bhsres <- bhs(X[,-1], y, RJ=FALSE, M=M, T=25000, verb = 0)}
+  bhsTime <- proc.time() - pt1
 }
 if ("bridge" %in% model) {
+  pt1 <- proc.time()
   if(exists(xtest)){bridgeres <- bridge(Xtest[,-1], ytest, RJ=FALSE, M=M, T=25000, verb = 0)} else {bridgeres <- bridge(X[,-1], y, RJ=FALSE, M=M, T=25000, verb = 0)}
+  briTime <- proc.time() - pt1
 }
-
+if ("lasso" %in% model){
+  pt1 <- proc.time()
+  if(exists(xtest)){lassores <- glmnet(Xtest[,-1], ytest, alpha=1,verb = 0)} else {lassores <- glmnet(X[,-1], y, verb = 0)}
+  lasTime <- proc.time() - pt1
+}
+  if ("ridge" %in% model){
+    pt1 <- proc.time()
+    if(exists(xtest)){ridgeres <- glmnet(Xtest[,-1], ytest, alpha=0, verb = 0)} else {ridgeres <- glmnet(X[,-1], y, RJ=FALSE, M=M, T=25000, verb = 0)}
+    ridgeTime <- proc.time() - pt1
+  }
+  if ("elnet" %in% model){
+    pt1 <- proc.time()
+    if(exists(xtest)){elnetres <- glmnet(Xtest[,-1], ytest, alpha=.5, verb = 0)} else {elnetres <- glmnet(X[,-1], y, verb = 0)} #I can specify the elasticnet mixing parameter between 1 and 0; I've defaulted to midway between
+    elnetTime <- proc.time() - pt1
+  }
+#I don't fully understand glmnet's "relax" option, so I've left it out. documentation says that this feature is best if fitting the original glmnet model takes a long time.
+  
 
 #predict y from models
+  #### need to add glmnet options
 if (withPred == TRUE) {
   
   if(exists("lmres")){
@@ -126,28 +163,49 @@ if (withPred == TRUE) {
     bridgepred <- mean(bridgeres$mu[10001:25000]) + Xval[,-1] %*% colMeans(bridgeres$beta[10001:25000,]) 
     bripredRMSE <- sqrt(mean((summary(lm(bripred~yval))$residuals)^2))
   }
+  if(exists("lassores")){
+    lassopred <- coef(lassores)[1] + Xval[,-1] %*% coef(lassores)[-1]
+    lassopredRMSE <- sqrt(mean((summary(lm(lassopred~yval))$residuals)^2))
+  }
+  if(exists("ridgeres")){
+    ridgepred <- coef(ridgeres)[1] + Xval[,-1] %*% coef(ridgeres)[-1]
+    ridgepredRMSE <- sqrt(mean((summary(lm(ridgepred~yval))$residuals)^2))
+  }
+  if(exists("elasres")){
+    elaspred <- coef(elasres)[1] + Xval[,-1] %*% coef(elasres)[-1]
+    elaspredRMSE <- sqrt(mean((summary(lm(elaspred~yval))$residuals)^2))
+  }
+  
 }
   
 # I think it would be more efficient to output performance metrics rather than model parameters, but I'm still working out how best to get comparable metrics for all the models
 
 # which model did best on the excluded data?
+  # note: this code isn't working the way I want it to, yet
 if (length(model) > 1 & withPred == TRUE){
-    leastErrorPred <- c("Linear","Susie","Bayesian Lasso")[which.min(c(if(exists("lmpredRMSE")){lmpredRMSE}, if(exists("suspredRMSE")){suspredRMSE}, if(exists("blaspredRMSE")){blaspredRMSE}))]
-  }	
+    leastErrorPred <- c("Linear","Susie","Bayesian Lasso", "Horseshoe Lasso", "Ridge Lasso")[which.min(c(if(exists("lmpredRMSE")){lmpredRMSE}, if(exists("suspredRMSE")){suspredRMSE}, if(exists("blaspredRMSE")){blaspredRMSE}, if(exists("bhspredRMSE")){bhspredRMSE}, if(exists("bripredRMSE")){bripredRMSE}))]
+}	
+  
+if (length(model) > 1){ # and which model was quickest?
+    leastTime <- c("Linear","Susie","Bayesian Lasso", "Horseshoe Lasso", "Ridge Lasso")[which.min(c(if(exists("lmTime")){lmTime}, if(exists("susTime")){susTime}, if(exists("blasTime")){blasTime}, if(exists("bhsTime")){bhsTime}, if(exists("briTime")){briTime}))]
+}	
   
 
 #get output
 out <- list(if(exists("ytest")){ytest}, if(exists("yval")){yval}, 
             if(exists("Xtest")){Xtest}, if(exists("Xtest")){Xtest}, 
-            if(exists("lmres")){coef(lmres)}, if(exists("lmpred")){c(lmpred, lmpredRMSE)},
+            if(exists("lmres")){coef(lmres)}, if(exists("lmpred")){c(lmpred, lmpredRMSE)}, 
             if(exists("susres")){coef(susres)}, if(exists("suspred")){c(suspred,suspredRMSE)},
             if(exists("blassores")){c(mean(blassores$mu[10001:25000]),colMeans(blassores$beta[10001:25000,]))}, if(exists("blassopred")){c(blassopred,blassopredRMSE)},
             # now, I'll have intercept and coefficients, in same form as lm and susie
             if(exists("bhsres")){c(mean(bhsres$mu[10001:25000]),colMeans(bhsres$beta[10001:25000,]))}, if(exists("bhspred")){c(bhspred,bhspredRMSE)},
             if(exists("bridgeres")){c(mean(bridgeres$mu[10001:25000]),colMeans(bridgeres$beta[10001:25000,]))}, if(exists("bridgepred")){c(bridgepred, bripredRMSE)}
+            if(exists("lassores")){coef(lassores)}, if(exists("lassopred")){c(lassopred, lassoRMSE)},
+            if(exists("ridge")){coef(ridgeres)}, if(exists("ridgepred")){c(ridgepred, ridgeRMSE)},
+            if(exists("elasticnet")){coef(elasres)}, if(exists("elaspred")){c(elaspred, elasRMSE)}
 )
 
-names(out) <- c("ytest", "yval", "Xtest", "Xval", "lmBeta", "lmPred", "lmpredRMSE", "susBeta", "susPred", "suspredRMSE", "blasBeta", "blasPred", "blaspredRMSE", "bhsBeta", "bhsPred", "bhspredRMSE", "bridgeBeta", "bridgePred", "brepredRMSE")
+names(out) <- c("ytest", "yval", "Xtest", "Xval", "lmBeta", "lmPred", "lmpredRMSE", "susBeta", "susPred", "suspredRMSE", "blasBeta", "blasPred", "blaspredRMSE", "bhsBeta", "bhsPred", "bhspredRMSE", "bridgeBeta", "bridgePred", "bridgepredRMSE", "lassoBeta", "lassoPred", "lassopredRMSE","ridgeBeta", "ridgePred", "ridgepredRMSE","elasBeta", "elasPred", "elaspredRMSE")
 out <- out[!sapply(out,is.null)]
 return(out)  
 }
